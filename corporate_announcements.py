8946da01a752cdc062016e1aa7c2125cc07ce957
Yogesh Kumar Bardia <yogesh.bardia@gmail.com>
2024-02-28 00:05:16 +0530

    added global exception handling

--- corporate_announcements.py
+++ corporate_announcements.py
@@ -6,9 +6,6 @@ import telegram as tele
 import sys
 from memory import Memory
 
-
-
-
 def today(diff):
     dt = date.today() - timedelta(days=diff)
     return dt.strftime("%d-%m-%Y");
@@ -32,36 +29,44 @@ if len(sys.argv) != 3:
 
 chatId = sys.argv[1]
 fileName = sys.argv[2]
-memory = Memory(fileName)
 
-mainline_stocks_announcements = announcements_for_equities()
-sme_stock_announcements = announcements_for_sme()
+try:   
+    memory = Memory(fileName)
 
-# Concat both the result of mainline stocks and sme stocks
-stock_announcements = mainline_stocks_announcements + sme_stock_announcements
+    mainline_stocks_announcements = announcements_for_equities()
+    sme_stock_announcements = announcements_for_sme()
 
-# Filter only event related announcements
-eventProcessor = EventProcessor()
-events=[]
-for announcement in stock_announcements:
-    if eventProcessor.apply(announcement):
-        if memory.isPresentInMemory(announcement['sm_name']) == False:
-            events.append(announcement)
-            memory.addToMemory(announcement['sm_name'])
+    # Concat both the result of mainline stocks and sme stocks
+    stock_announcements = mainline_stocks_announcements + sme_stock_announcements
 
-# events = filter(lambda x: eventProcessor.apply(x), stock_announcements)
-print('Total filtered events', len(events))
-print('Total filtered events desc', [event['desc'] for event in events])
-pp = pprint.PrettyPrinter(indent=4)
+    # Filter only event related announcements
+    eventProcessor = EventProcessor()
+    events=[]
+    for announcement in stock_announcements:
+        if eventProcessor.apply(announcement):
+            if memory.isPresentInMemory(announcement['sm_name']) == False:
+                events.append(announcement)
+                memory.addToMemory(announcement['sm_name'])
 
-for event in events:
-    # Format the message
-    formatted_message = tele.format_message(event['sm_name'], event['attchmntText'], 'Attachment Report',event['attchmntFile'])
+    # events = filter(lambda x: eventProcessor.apply(x), stock_announcements)
+    print('Total filtered events', len(events))
+    print('Total filtered events desc', [event['desc'] for event in events])
+    pp = pprint.PrettyPrinter(indent=4)
 
+    for event in events:
+        # Format the message
+        formatted_message = tele.format_message(event['sm_name'], event['attchmntText'], 'Attachment Report',event['attchmntFile'])
+
+        # Send the message to Telegram
+        tele.send_telegram_message(chatId, formatted_message, parse_mode='HTML')
+
+
+    #print('Total filtered events', events)
+    pp.pprint(events)
+    memory.saveMemory()
+
+except Exception as e:
+    error_message = e.args[0]
+    print('######',error_message)
     # Send the message to Telegram
-    tele.send_telegram_message(chatId, formatted_message, parse_mode='HTML')
-
-
-#print('Total filtered events', events)
-pp.pprint(events)
-memory.saveMemory()
+    tele.send_telegram_message(chatId, '<h2>Stock Announcement Script error<h2>\n'+error_message, parse_mode='HTML')
